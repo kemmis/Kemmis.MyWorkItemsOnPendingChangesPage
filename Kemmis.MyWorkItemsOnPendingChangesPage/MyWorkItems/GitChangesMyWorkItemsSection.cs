e4522b176fa9731a7cd81290c70b9fabde6a8c86
Rafe Kemmis <rafe@kemmis.info>
2018-10-05 05:02:46 -0400

    changed priority/placement of section on git changes page
    https://github.com/kemmis/Kemmis.MyWorkItemsOnPendingChangesPage/issues/18

--- Kemmis.MyWorkItemsOnPendingChangesPage/MyWorkItems/GitChangesMyWorkItemsSection.cs
+++ Kemmis.MyWorkItemsOnPendingChangesPage/MyWorkItems/GitChangesMyWorkItemsSection.cs
@@ -2,7 +2,7 @@
 
 namespace Kemmis.MyWorkItemsOnPendingChangesPage.MyWorkItems
 {
-    [TeamExplorerSection(SectionId, TeamExplorerPageIds.GitChanges, 35)]
+    [TeamExplorerSection(SectionId, TeamExplorerPageIds.GitChanges, 25)]
     internal class GitChangesMyWorkItemsSection : MyWorkItemsSection
     {
         public const string SectionId = "4C82595C-9E77-467E-9F25-D886E694C999";
